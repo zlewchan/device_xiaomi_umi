930abf6dfdbfab026536d5fb8f442bcd3e87a450
x0x00044 <zlewchan@outlook.com>
2023-07-12 15:07:03 +0200

    Revert "umi: Go away Megvii"
    This reverts commit 89819d8c423d25eb7e118a245065dadf72ffae9a.

--- device.mk
+++ device.mk
@@ -144,10 +144,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libMegviiFacepp-0.5.2 \
-    libmegface
-
-PRODUCT_PACKAGES += \
     libpiex_shim
 
 # Component overrides
